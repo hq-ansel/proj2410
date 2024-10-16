c9310fdfd2754d3d24963fde341b7442171b3d99
heqi-ansel <18175731485@163.com>
2024-10-16 19:33:19 +0800

    新增bitdistiller 给quantlinear增加一个参数

--- EfficientQAT/launch.sh
+++ EfficientQAT/launch.sh
@@ -30,12 +30,12 @@
     # 指定数据集位置
     export HF_HOME="/home/ubuntu/data/exp/proj2410/hf_home"
     export WINDOW_SIZE=1
-    export CUDA_VISIBLE_DEVICES=0  # or e.g. 0,1,2,3
+    export CUDA_VISIBLE_DEVICES=1  # or e.g. 0,1,2,3
     export MODEL_PATH=/home/ubuntu/data/exp/proj2410/model/Llama2-7b
     export DATASET_PATH=pajama
     export AMP_ENABLED=True
     export SAVE_PATH=/home/ubuntu/data/exp/proj2410/quant_model/EfficientQAT/w4gs128/Llama2-7b
-    export CONFIG_PATH=/home/ubuntu/data/exp/proj2410/EfficientQAT/yaml/b2gs128-mad.yaml
+    export CONFIG_PATH=/home/ubuntu/data/exp/proj2410/EfficientQAT/yaml/b4gs128.yaml
 
     python -m EfficientQAT.main_block_ap  \
     --config_path $CONFIG_PATH \

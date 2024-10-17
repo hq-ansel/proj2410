814a46a8f9a39ef2648d526dba7db66c48a82e9b
heqi-ansel <18175731485@163.com>
2024-10-17 19:27:50 +0800

    微调到8*a6000能够接受

--- BitDistiller/train/train.sh
+++ BitDistiller/train/train.sh
@@ -60,8 +60,8 @@ torchrun --nproc_per_node=8 --master_port=$MASTER_PORT train.py \
     --num_train_epochs $4 \
     --bf16 True \
     --seed 42 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 16 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --gradient_checkpointing True \
     --evaluation_strategy "steps" \

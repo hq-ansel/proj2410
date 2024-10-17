db79e10ab6a6a8a42d660857392b3a9ab3a37713
heqi-ansel <18175731485@163.com>
2024-10-17 13:15:01 +0800

    验证过即使用所有的节点也没用

--- BitDistiller/train/launch.sh
+++ BitDistiller/train/launch.sh
@@ -4,7 +4,6 @@
     # 指定数据集位置
     export HF_HOME="/home/ubuntu/data/exp/proj2410/hf_home"
     export MODEL_PATH=/home/ubuntu/data/exp/proj2410/model/Llama2-7b
-    export CUDA_VISIBLE_DEVICES=4,5,6,7
 
     bash /home/ubuntu/data/exp/proj2410/BitDistiller/train/train.sh \
     /home/ubuntu/data/exp/proj2410/BitDistiller/data_gen/datasets/hf-llama-2-7b/mix_wiki_alpaca_8000.json \

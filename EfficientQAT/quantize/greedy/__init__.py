bf03f95d65a8986ef9e142ff829bc9226ca6f9d6
heqi-ansel <18175731485@163.com>
2025-11-12 00:42:21 +0800

    Normalize all EfficientQAT import blocks to pylint’s preferred order (stdlib / third-party / local) so that linting stops flagging wrong-import-order.

---
+++ EfficientQAT/quantize/greedy/__init__.py
@@ -0,0 +1,3 @@
+from .common import CosineAnnealingScheduler, CatcherManager, CommonInputDataset  # noqa: F401
+from .pipeline import GreedyBlockPipeline, trans_quant_block  # noqa: F401
+from .training import train_units_layers, train_units_layers_with_catcher  # noqa: F401
